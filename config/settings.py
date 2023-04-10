59a39e7c759f8bc878cd50a68f2c0bf99a58f881
sparkyPythoneer <jibunoh.augustine@gmail.com>
2023-04-10 16:54:12 +0100

    UPDATE[MODIFY CONFIG III]

--- config/settings.py
+++ config/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': config(
-        'DATABASE_URL',
-        default='postgres://postgres:postgres@localhost:5432/github_actions',
-        cast=db_url
-    )
+    'default': {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASS"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT")
+    }
 }
 
 
